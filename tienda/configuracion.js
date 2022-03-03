4f615dfeab049fc998417acfa4b778c1aa9438b5
lmarino12 <lmarino@est.ups.edu.ec>
2022-03-03 15:29:32 -0500

    Update configuracion.js
    port

--- tienda/configuracion.js
+++ tienda/configuracion.js
@@ -1,6 +1,6 @@
 const config = {
     DB_URL: 'mongodb+srv://ups:ups2020@cluster0.pjyad.mongodb.net/tienda?retryWrites=true&w=majority',
-    PUERTO: 3000
+    PUERTO: 3004
 }
 
 module.exports = config
